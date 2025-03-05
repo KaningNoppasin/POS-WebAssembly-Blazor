2a6d4eb654945898c8d7cbc4a7e249532a95adb6
NN <95127494+KaningNoppasin@users.noreply.github.com>
2025-03-05 23:21:11 +0700

    feat: product work only one time #fail

--- BlazorWebAssembly.Client/Program.cs
+++ BlazorWebAssembly.Client/Program.cs
@@ -4,5 +4,8 @@ using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddMudServices();
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080/api/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 
 await builder.Build().RunAsync();
